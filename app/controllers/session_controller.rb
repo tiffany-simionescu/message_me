8d312ee23482ccdeebbee732ba88fb1f1df5048d
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-08-21 12:32:49 -0400

    Ability to login and logout.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,7 +1,23 @@
 class SessionController < ApplicationController
 
   def new
-
   end
   
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id 
+      flash[:success] = "You have successfully logged in"
+      redirect_to root_path
+    else  
+      flash.now[:error] = "Incorrect login information. Please try again"
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil 
+    flash[:success] = "You have successfully logged out"
+    redirect_to login_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'chatroom#index'
   get 'login', to: 'session#new'
+  post 'login', to: 'session#create'
+  delete 'logout', to: 'session#destroy'
 end

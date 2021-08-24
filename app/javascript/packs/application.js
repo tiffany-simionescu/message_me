79d00fcbaca867dae6a776dee9a30921cf55753a
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2021-08-24 10:20:41 -0400

    Updated functionality of input.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
+import 'jquery'
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
@@ -13,9 +14,27 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+window.scroll_bottom = function() {
+  if ($('#messages').length > 0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
+window.submit_message = function() {
+  $('#message_body').on('keydown', function(e) {
+    if (e.keyCode == 13) {
+      e.preventDefault(); 
+      $('button').click();
+      e.target.value = "";
+    };
+  });
+};
+
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+  submit_message();
+  scroll_bottom();
 })
